79c9c41279131f3f65f80d80069bcbcc617f97bd
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-05-26 01:15:23 -0400

    Add in some basic text replacement as well

--- sparkpost.php
+++ sparkpost.php
@@ -250,8 +250,10 @@ function sparkpost_civicrm_alterMailParams(&$params, $context) {
   if (stripos($params['from'], 'do-not-reply') && CRM_Sparkpost::isHoffer()) {
     $params['from'] = '"Doug Hoffer for Auditor" <do-not-reply@hofferforauditor.com>';
     $params['replyTo'] = $params['returnPath'] = 'do-not-reply@hofferforauditor.com';
+    $params['html'] = str_replace('Vermont Progressive Party', 'Doug Hoffer for Auditor', $params['html']);
     $params['html'] = str_replace('zuckermanforvt.jmaconsulting.biz', 'hoffer.jmaconsulting.biz', $params['html']);
     $params['html'] = str_replace('mail.zuckermanforvt.com', 'crm.hofferforauditor.com', $params['html']);
+    $params['text'] = str_replace('Vermont Progressive Party', 'Doug Hoffer for Auditor', $params['text']);
     $params['text'] = str_replace('zuckermanforvt.jmaconsulting.biz', 'hoffer.jmaconsulting.biz', $params['text']);
     $params['text'] = str_replace('mail.zuckermanforvt.com', 'crm.hofferforauditor.com', $params['text']);
   }
